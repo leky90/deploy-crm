ef993902acf5b945f8cae46c876534ffea930d8b
Ky Le <ky@treehouse.finance>
2024-03-19 09:35:55 +0700

    update user status endpoint

---
+++ assets/cell-link-BAnpjPcp.js
@@ -0,0 +1,1 @@
+import{j as s,a6 as i}from"./index-DjsK4lwM.js";function l({children:e,to:n}){return s.jsxDEV(i,{className:"truncate",to:n,children:e||"-"},void 0,!1,{fileName:"/Users/ldky90/Public/Training/crm/src/app/components/cell-link.tsx",lineNumber:8,columnNumber:5},this)}export{l as C};
