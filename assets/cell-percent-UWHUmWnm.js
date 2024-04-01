c3b7e83ea87683f34a73c2cda46422d1c33c3985
Ky Le <ky@treehouse.finance>
2024-04-01 11:28:26 +0700

    update final feedback

---
+++ assets/cell-percent-UWHUmWnm.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BwT9pd9i.js";import{f as t}from"./format-percent-Ca7gawAv.js";function c({value:e}){return r.jsxDEV("div",{className:"truncate",children:t(e)},void 0,!1,{fileName:"/Users/ldky90/Public/Training/crm/src/app/components/cell-percent.tsx",lineNumber:8,columnNumber:10},this)}export{c as C};
---
+++ assets/cell-text-DkpivG0U.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BwT9pd9i.js";import{d as t}from"./index-C2_1S1yO.js";function l({children:e,className:r}){return s.jsxDEV("div",{className:t("truncate",r),children:e||"-"},void 0,!1,{fileName:"/Users/ldky90/Public/Training/crm/src/app/components/cell-text.tsx",lineNumber:9,columnNumber:10},this)}export{l as C};
