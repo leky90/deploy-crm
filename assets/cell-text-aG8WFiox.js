b94095bb42b403e8b37807cd403240bb72f12530
Ky Le <ky@treehouse.finance>
2024-04-01 22:37:31 +0700

    update startup app

---
+++ assets/cell-text-aG8WFiox.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-iY8wkAMF.js";import{d as t}from"./index-Bi7vddj2.js";function l({children:e,className:r}){return s.jsxDEV("div",{className:t("truncate",r),children:e||"-"},void 0,!1,{fileName:"/Users/ldky90/Public/Training/crm/src/app/components/cell-text.tsx",lineNumber:9,columnNumber:10},this)}export{l as C};
