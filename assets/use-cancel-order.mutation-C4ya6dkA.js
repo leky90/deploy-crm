d1f39109ea5d9ea1a01d36fa79fc6d0f57739781
Ky Le <ky@treehouse.finance>
2024-04-04 17:28:52 +0700

    update build

---
+++ assets/use-cancel-order.mutation-C4ya6dkA.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useMutation-olpSmLLZ.js";import{a as r}from"./Helmet-BSeNyQbR.js";import{a as n,Q as o}from"./index-D1JUiLua.js";function a(t){return r({method:"post",url:"/customer_history/order_cancel",data:t})}function c(){const{mutateAsync:t}=e({mutationFn:a,onSuccess:()=>{n.invalidateQueries({queryKey:o.CUSTOMER_HISTORY})}});return{mutationCancelOrder:t}}export{c as u};
