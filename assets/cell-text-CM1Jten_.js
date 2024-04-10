b5d5026e4ed3b313e6cea78ffd70a8f3a7c8bea5
Ky Le <ky@treehouse.finance>
2024-04-10 17:01:20 +0700

    update style

---
+++ assets/cell-text-CM1Jten_.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CJ4aojTS.js";import{d as t}from"./button-C7e1d5xZ.js";function l({children:e,className:r}){return s.jsxDEV("div",{className:t("truncate",r),children:e||"-"},void 0,!1,{fileName:"/Users/ldky90/Public/Training/crm/src/app/components/cell-text.tsx",lineNumber:9,columnNumber:10},this)}export{l as C};
